cefb52bf698c9d6c69bb09f4b28ec6e496db4be1
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-11-21 22:14:57 +0100

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,14 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "kohanyirobert"
 
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+  #   workspaces {
+  #     project = "Example"
+  #     name    = "learn-terraform"
+  #   }
+  # }
 
   required_providers {
     aws = {
